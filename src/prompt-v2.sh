c57ddeab396cd517158462373e3ea82e558911f9
Bricktech <bricktech2000@gmail.com>
2022-02-01 01:48:24 -0500

    modified unicode characters used

--- src/prompt-v2.sh
+++ src/prompt-v2.sh
@@ -47,39 +47,38 @@ export PS1='`
     if (( folder_count > 4 && $folder_count <= 8 )); then printf "⣷"; fi;
     if (( folder_count > 8 ));                       then printf "⣿"; fi;
   fi;
-  printf "\[\033[0m\]";
+  printf "\[\033[0m\] ";
 
   if [[ $UID -eq 0 ]];
     then printf "\[\033[1m\033[38;5;015m\]🗲 \[\033[0m\]";
     # else printf "\[\033[1m\033[38;5;008m\]\[\033[0m\]";
   fi;
 
-  if [[ $status -gt 0 ]];
-    then printf "\[\033[1m\033[91m\]✗";
-    # else printf "\[\033[1m\033[38;5;015m\]✓";
-  fi;
-  printf "\[\033[0m\]";
-
   running_jobs="\`jobs -l | wc -l;\`"
   if [[ $running_jobs -gt 1 ]];
-    then printf "\[\033[38;5;012m\]•\[\033[0m\]";
+    then printf "\[\033[1m\033[38;5;012m\]○ \[\033[0m\]";
   fi;
   process_running=$({ pgrep -x $CLEAN_PROMPT_CHECK_PROC; } | wc -l);
   if [[ $process_running -eq 0 ]];
-    then printf "\[\033[1m\033[91m\]×\[\033[0m\]";
+    then printf "\[\033[1m\033[91m\]× \[\033[0m\]";
   fi;
+
+  if [[ $status -gt 0 ]];
+    then printf "\[\033[1m\033[91m\]✗ ";
+    # else printf "\[\033[1m\033[38;5;015m\]✓ ";
+  fi;
+  printf "\[\033[0m\]";
 
   git branch --show-current &> /dev/null;
   if [[ $? -eq 0 ]];
     then status=$(git status 2> /dev/null);
     if [[ $status =~ Changes\ not\ staged\ for\ commit: ]] || [[ $status =~ Untracked\ files: ]];
-      then printf "\[\033[38;5;226m\]🡲 \[\033[0m\]";
+      then printf "\[\033[38;5;226m\]⯈ \[\033[0m\]";
     elif [[ $status =~ Changes\ to\ be\ committed: ]];
-      then printf "\[\033[38;5;040m\]🡲 \[\033[0m\]";
+      then printf "\[\033[38;5;040m\]⯈ \[\033[0m\]";
     else
-      printf "\[\033[94m\]🡲 \[\033[0m\]";
+      printf "\[\033[1m\033[015m\]⯈ \[\033[0m\]";
     fi;
-  else printf " ";
   fi;
 `'
 export PS2=' '
